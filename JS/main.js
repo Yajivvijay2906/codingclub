cb205dd677604531ea6affd18494a3ff15f77d2d
Yajivvijay2906 <91339707+Yajivvijay2906@users.noreply.github.com>
2021-09-24 22:21:37 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,108 @@
+// Initialize and add the map
+function initMap() {
+    // Your location
+    const loc = { lat: 11.030975326522793, lng: 76.94566797916522 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+
+  // Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.8;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
+
+  
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+
+  // 11.030975326522793, 76.94566797916522
+
+
+  // Modal Animation
+
+  // Get the modal
+// var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+
+
+// Office bearers animation
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
